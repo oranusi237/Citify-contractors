cdfee8c118165ba71c4dccc50194cd2f4ca379db
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-17 11:12:55 +0100

    Added route for sign up and login page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { Box, Heading } from '@chakra-ui/react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './Components/Pages/Home'
 import About from './Components/Pages/About/About'
 import Contact from './Components/Pages/Contact/Contact'
 import './App.css';
+import SignuPage from './Components/Pages/Auth/SignUp/SignUp';
+import LoginPage from './Components/Pages/Auth/SignIn/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-                <Route path="/" element= {<Home />}/>
-                <Route path="/about" element= {<About />}/>
-                <Route path="/contact" element= {<Contact />}/>
-                <Route path="" />
-                <Route path="" />
-            </Routes>
-            </BrowserRouter>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="sign-up" element={<SignuPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
