bd41192ae842ff41870197f6f704708f52b10238
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-17 11:13:14 +0100

    Creating sign up component

---
+++ src/Components/Pages/Auth/SignUp/SignUp.jsx
@@ -0,0 +1,9 @@
+import { Box, Heading } from "@chakra-ui/react"
+
+export default function SignuPage() {
+    return (
+        <Box>
+            <Heading>You sign up goes here</Heading>
+        </Box>
+    )
+}
