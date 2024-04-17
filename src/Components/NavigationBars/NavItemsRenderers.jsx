b61508f368e91e0758e9d73d52785c7d7aab5221
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-17 07:31:41 +0100

    Initial commit

---
+++ src/Components/NavigationBars/NavItemsRenderers.jsx
@@ -0,0 +1,20 @@
+import { Stack, Heading, } from "@chakra-ui/react"
+import { navItems } from "./navItems";
+import { NavLink } from "react-router-dom";
+import "./nav.css"
+
+export default function NavItemsRenderers(direction) {
+    return (
+        <Stack direction={direction} gap={5}>
+            {navItems.map((item) => (
+                <NavLink className={({ isActive }) =>
+                    isActive ? "active" : "in-active"
+                } key={item.id} to={item.path}>
+                    <Heading color="white" fontSize={20}>
+                        {item.name}
+                    </Heading>
+                </NavLink>
+            ))}
+        </Stack>
+    );
+}
