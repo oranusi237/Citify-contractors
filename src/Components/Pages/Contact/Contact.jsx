50c4e9fe8c70e02039d79fb2a8bbaec998cbec92
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-18 11:03:48 +0100

    Adding mailto to email

--- src/Components/Pages/Contact/Contact.jsx
+++ src/Components/Pages/Contact/Contact.jsx
@@ -1,21 +1,23 @@
 import { Box, Flex, FormControl, FormLabel, Heading, Input, Stack, Text, Textarea } from "@chakra-ui/react";
 import CustomSolidButton from "../../Reuseables/Solidbutton";
+import { Link } from "react-router-dom";
+import { primaryColor } from "../../Reuseables/colors";
 
 export default function ContactPage() {
     return (
         <Box>
             <Flex wrap={"wrap"} align="start" padding={10}>
                 <Stack padding={10}>
-                    <Text lineHeight={1.8} fontSize="18px">Welcome to City Contractors, where your satisfaction is our utmost priority. We are dedicated to fostering open communication and are always eager to assist you. Whether you have inquiries regarding our comprehensive range of services, wish to provide feedback, or simply want to connect, we invite you to reach out. 
+                    <Text lineHeight={1.8} fontSize="18px">Welcome to City Contractors, where your satisfaction is our utmost priority. We are dedicated to fostering open communication and are always eager to assist you. Whether you have inquiries regarding our comprehensive range of services, wish to provide feedback, or simply want to connect, we invite you to reach out.
 
-                        Please feel free to contact us by completing the form below. Alternatively, you can email us directly at contact@citycontractors.com. Our committed team is devoted to delivering prompt and informative responses to all your inquiries.
+                        Please feel free to contact us by completing the form below. Alternatively, you can email us directly at <Link style={{ color: primaryColor }} to="mailto:contact@citycontractors">contact@citycontractors.com</Link>. Our committed team is devoted to delivering prompt and informative responses to all your inquiries.
 
                         We recognize the value of dependable communication and eagerly anticipate the opportunity to serve you. Thank you for considering City Contractors for your needs. We look forward to connecting with you soon!
                     </Text>
                 </Stack>
                 <form>
                     <Stack padding={10} width={["90vw", "80vw", "60vw"]}>
-                        <Heading>Contact us</Heading>
+                        <Heading>Send us a message</Heading>
                         <FormControl>
                             <FormLabel>Full name</FormLabel>
                             <Input type="tex" />
