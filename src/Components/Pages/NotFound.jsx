223de1b89f67d36ea44f92e28b881ca777c899da
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-19 05:26:21 +0100

    Adding button to home on 404 page

--- src/Components/Pages/NotFound.jsx
+++ src/Components/Pages/NotFound.jsx
@@ -1,12 +1,19 @@
 import { Center, Heading, Stack, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import CustomSolidButton from "../Reuseables/Solidbutton";
 
 export default function PageNotFound() {
     return (
-        <Center minH={"80vh"} textAlign="center">
-            <Stack>
-                <Heading>404 error!</Heading>
-                <Text>The page you are looking for cannot be found</Text>
-            </Stack>
-        </Center>
+        <>
+            <Center minH={"80vh"} textAlign="center">
+                <Stack>
+                    <Heading>404 error!</Heading>
+                    <Text>The page you are looking for cannot be found</Text>
+                    <Link to="/">
+                        <CustomSolidButton buttonText="Go home" />
+                    </Link>
+                </Stack>
+            </Center>
+        </>
     )
 }
