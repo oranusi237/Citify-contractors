955ad5260e9cec71d51bd303e679c207aeed9879
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-19 18:58:17 +0100

    Updating pricing package styles

--- src/Components/Reuseables/Cards/PricingCard.jsx
+++ src/Components/Reuseables/Cards/PricingCard.jsx
@@ -1,26 +1,32 @@
 import { Box, Stack, ListItem, List, Heading, Text } from "@chakra-ui/react";
 import CustomSolidButton from "../Solidbutton";
+import { primaryColor } from "../colors";
 
 export default function PricingCard({ planName, planDuration, planPrice, numberOfFeatures, feature1, feature2, feature3, feature4, feature5,
-    feature6, feature7, feature8, feature9, feature10 }) {
+    feature6, feature7, feature8, feature9 }) {
+
+    const goldPlan = typeof (planName) === "string" && planName.toString().toLowerCase().includes("gold")
+
     return (
         <Box>
-            <Stack textAlign="center" borderRadius={10} padding="15px" height="max-content" background="#EB4C15" width="300px">
-                <Heading color="whitesmoke">Plan Name</Heading>
-                <Text color="whitesmoke">Plan Duration</Text>
-                <Heading color="whitesmoke">₦100,000</Heading>
-                <List color="white">
-                    <ListItem>Feature one</ListItem>
-                    <ListItem>Feature 2</ListItem>
-                    <ListItem>Feature 3</ListItem>
-                    <ListItem>Feature 4</ListItem>
-                    <ListItem>Feature 5</ListItem>
-                    <ListItem>Feature 6</ListItem>
-                    <ListItem>Feature 7</ListItem>
-                    <ListItem>Feature 8</ListItem>
+            <Stack margin textAlign="center" borderRadius={10} padding="15px" height="max-content" background={goldPlan ? "#EB4C15" : "white"}
+                boxShadow={goldPlan ? "" : "0px 0px 12px 13px rgba(226,226,226,0.54)"} width="300px" >
+                <Heading color={goldPlan ? "whitesmoke" : primaryColor}>{planName}</Heading>
+                <Text fontWeight={600} color={goldPlan ? "whitesmoke" : primaryColor}>{planDuration}</Text>
+                <Heading color={goldPlan ? "whitesmoke" : primaryColor}>₦{planPrice}</Heading>
+                <List color={goldPlan ? "whitesmoke" : primaryColor}>
+                    <ListItem>{feature1}</ListItem>
+                    <ListItem>{feature2}</ListItem>
+                    <ListItem>{feature3}</ListItem>
+                    <ListItem>{feature4}</ListItem>
+                    <ListItem>{feature5}</ListItem>
+                    <ListItem>{feature6}</ListItem>
+                    <ListItem>{feature7}</ListItem>
+                    <ListItem>{feature8}</ListItem>
+                    <ListItem>{feature9}</ListItem>
                 </List>
-                <CustomSolidButton hoverColor="#ffff" textColor="#EB4C15" buttonColor="#F7F7F7" buttonText={"Choose plan"} />
+                <CustomSolidButton hoverColor={goldPlan && "#ffff"} textColor={goldPlan ? "#EB4C15" : "white"} buttonColor={!goldPlan ? primaryColor : "#F7F7F7"} buttonText={"Choose plan"} />
             </Stack>
-        </Box>
+        </Box >
     )
 }
