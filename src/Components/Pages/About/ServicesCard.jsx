e68f2ce96901845bc248906cd39a89bc6fa5df0c
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-19 20:43:24 +0100

    Updated card description element

--- src/Components/Pages/About/ServicesCard.jsx
+++ src/Components/Pages/About/ServicesCard.jsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Stack, Icon } from "@chakra-ui/react";
+import { Box, Heading, Stack, Icon, Text } from "@chakra-ui/react";
 import { primaryColor } from "../../Reuseables/colors";
 
 export default function ServicesCard({ cardIcon, description, serviceName }) {
@@ -7,7 +7,7 @@ export default function ServicesCard({ cardIcon, description, serviceName }) {
             <Stack bg="white" borderRadius={8} padding={"20px"} boxShadow={"-1px 1px 5px 1px rgba(192,198,165,0.75)"} justify="center" align="center" textAlign="center" width={["90%", "90%", "300px"]}>
                 <Icon fontSize={50} as={cardIcon} alt="service description image" />
                 <Heading fontWeight="900" color={primaryColor} fontSize="25px">{serviceName}</Heading>
-                <Heading fontWeight={200} fontSize="17px">{description}</Heading>
+                <Text fontWeight={200} fontSize="17px">{description}</Text>
             </Stack>
         </Box>
     )
