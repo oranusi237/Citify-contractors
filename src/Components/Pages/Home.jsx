c97372acddcafd7d438c462a16457ad4d839776f
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-21 21:40:15 +0100

    Reverted home code

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -26,54 +26,37 @@ export default function Homepage() {
       <HeroSection buttonText={"Find out more"} backgroundImage={Bg} headingText={"Business"} heroDescription="We help you achieve your goals" />
 
       <Box mt={5} >
-        <Stack direction={{base: 'column', md: 'row'}}>
-          <Box bgImage={Bg1} bgSize={"cover"} bgPosition={"center"} flex={1} height="300px" borderRadius="5px"></Box>
-          <Box bg="#282C35" color="white" p={4} flex={1} borderRadius="5px">
-            <Flex alignItems={"center"} flexDirection={"column"} textAlign={"center"}>
-        <Heading as="h2">About Us</Heading>
-        <Text fontSize="lg">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dolorum ipsum et beatae molestias ut 
-          dignissimos aliquid reprehenderit assumenda, quidem, nihil ducimus vitae. Debitis maxime modi animi 
-          voluptatum autem corporis.
-        </Text>
-        <Button marginTop="5px" >
-          <a href="/About">Read More</a>
-          </Button>
-        </Flex>
-        </Box>
+        <Stack align="center" direction={{ base: 'column', md: 'row' }}>
+          <Box bgImage={Bg1} bgSize={"cover"} bgPosition={"center"} flex={1} height="500px" borderRadius="5px"></Box>
+          <Box p={4} flex={1} borderRadius="5px">
+            <Flex alignItems={"center"} flexDirection={"column"} textAlign={"start"}>
+              <Stack >
+                <Heading fontSize="20px">About us</Heading>
+                <Heading fontSize="25px">Why choosing us is best for you</Heading>
+                <Text>We are a dedicated team of professionals committed to excellence in every project we undertake. With years of experience and a passion for what we do, we strive to deliver top-notch services that meet our clients' unique needs and exceed their expectations. Our team's expertise, combined with our unwavering commitment to quality and customer satisfaction, sets us apart as a trusted name in our industry.</Text>
+                <Link to="/about">
+                  <CustomSolidButton buttonWidth="200px" buttonText="Find more about us" />
+                </Link>
+              </Stack>
+            </Flex>
+          </Box>
         </Stack>
       </Box>
 
-      <Box p={4} mt={5} background="#282C35" color="white" height={300} borderRadius="20px">
-      <Box>
-      <Center flexDirection={"column"} flexWrap={"wrap"}>
-        <Text textAlign={"center"}>What You Get</Text>
-        <Heading textAlign={"center"}>Our Services</Heading>
-        <HStack spacing={12} textAlign={"center"}>
-          <Box>
-          <Box display={"inline-flex"}> <FaLandmark color="#00840B" size={60}/></Box>
-          <Text> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet 
-          dolorum ipsum et beatae molestias dignissimos aliquid reprehenderit assumenda, quidem, nihil ducimus vitae.
-           Debitis maxime modi animi voluptatum autem corporis.
-         </Text>
-          </Box>
-          <Box>
-           <Box display={"inline-flex"}> <FaLandmark color="#00840B" size={60}/></Box>
-          <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet 
-          dolorum ipsum et beatae molestias dignissimos aliquid reprehenderit assumenda, quidem, nihil ducimus vitae.
-           Debitis maxime modi animi voluptatum autem corporis.
-          </Text>
-          </Box>
-          <Box>
-          <Box display={"inline-flex"} > <FaLandmark color="#00840B" size={60}/></Box>
-          <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet 
-          dolorum ipsum et beatae molestias dignissimos aliquid reprehenderit assumenda, quidem, nihil ducimus vitae.
-           Debitis maxime modi animi voluptatum autem corporis.
-          </Text>
-          </Box>
-        </HStack>
+      <Spacer height={50} />
+
+      <Box p={4} mt={5} background="#282C35" minH={"max-content"} borderRadius="20px">
+        <Center py={10} gap={3} flexDirection={"column"} flexWrap={"wrap"}>
+          <Text color="white" textAlign={"center"}>What You Get</Text>
+          <Heading color="white" textAlign={"center"}>Our Services</Heading>
+
+          <Flex gap={10}>
+            <ServicesCard serviceName={"Real estate management"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eius, soluta exercitationem ducimus inventore saepe nostrum placeat temporibus, dolorum maxime vero sapiente tempore rerum enim eum ab architecto, commodi distinctio."} cardIcon={BsHouse} />
+
+            <ServicesCard serviceName={"Real estate management"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eius, soluta exercitationem ducimus inventore saepe nostrum placeat temporibus, dolorum maxime vero sapiente tempore rerum enim eum ab architecto, commodi distinctio."} cardIcon={BsHouse} />
+
+            <ServicesCard serviceName={"Real estate management"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eius, soluta exercitationem ducimus inventore saepe nostrum placeat temporibus, dolorum maxime vero sapiente tempore rerum enim eum ab architecto, commodi distinctio."} cardIcon={BsHouse} />
+          </Flex>
         </Center>
       </Box>
 
