e451bf096cd2c756774bec5582a208ca233f1815
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-21 21:24:29 +0100

    Updating layout and adding content

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,83 +1,119 @@
-import { Box, Heading, Text, Stack, HStack, Center, Button, Flex,  } from "@chakra-ui/react";
+import { Box, Heading, Text, Stack, Center, Accordion, AccordionItem, AccordionPanel, AccordionButton, AccordionIcon, Button, Flex, Spacer, } from "@chakra-ui/react";
 import Bg from "../Assets/Images/pexel1.jpg"
 import Bg1 from "../Assets/Images/pexels-icsa-833425-1709003.jpg"
-import { FaLandmark } from "react-icons/fa6";
+import HeroSection from "../Reuseables/HeroSection";
+import CustomSolidButton from "../Reuseables/Solidbutton";
+import { Link } from "react-router-dom";
+import ServicesCard from "./About/ServicesCard";
+import { BsHouse } from "react-icons/bs";
+import { primaryColor } from "../Reuseables/colors";
 
 export default function Homepage() {
+
+  function CustomAccordionButton({ label, itemNumber }) {
     return (
-        <div>
-         <Box p={4} bgImage={Bg} bgSize={"cover"} backgroundPosition={"center"} height="500px">
-         <Flex  justifyContent={"Center"}>
-      <Stack spacing={8} color="#fff">
-        <Center>
-          <Heading> Business</Heading>
-        </Center>
-        <Text fontSize="25px">We help you achieve your goals.</Text>
-      </Stack>
-      </Flex>
-      </Box>
-      <Box mt={5} >
-        <Stack direction={{base: 'column', md: 'row'}}>
-          <Box bgImage={Bg1} bgSize={"cover"} bgPosition={"center"} flex={1} height="300px" borderRadius="5px"></Box>
-          <Box bg="#282C35" color="white" p={4} flex={1} borderRadius="5px">
-            <Flex alignItems={"center"} flexDirection={"column"} textAlign={"center"}>
-        <Heading as="h2">About Us</Heading>
-        <Text fontSize="lg">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dolorum ipsum et beatae molestias ut 
-          dignissimos aliquid reprehenderit assumenda, quidem, nihil ducimus vitae. Debitis maxime modi animi 
-          voluptatum autem corporis.
-        </Text>
-        <Button marginTop="5px" >
-          <a href="/About">Read More</a>
-          </Button>
+      <AccordionButton>
+        <Flex fontWeight={500} fontSize="23px" gap={3} justify="start" align="center" flex={1} textAlign='left'>
+          <Center color="white" fontWeight={800} textAlign="center" bg={primaryColor} borderRadius={"50%"} height={"30px"} width={"30px"}>{itemNumber}</Center>{label}
         </Flex>
-        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    )
+  }
+
+  return (
+    <Box>
+      <HeroSection buttonText={"Find out more"} backgroundImage={Bg} headingText={"Business"} heroDescription="We help you achieve your goals" />
+
+      <Box mt={5} >
+        <Stack align="center" direction={{ base: 'column', md: 'row' }}>
+          <Box bgImage={Bg1} bgSize={"cover"} bgPosition={"center"} flex={1} height="500px" borderRadius="5px"></Box>
+          <Box p={4} flex={1} borderRadius="5px">
+            <Flex alignItems={"center"} flexDirection={"column"} textAlign={"start"}>
+              <Stack >
+                <Heading fontSize="20px">About us</Heading>
+                <Heading fontSize="25px">Why choosing us is best for you</Heading>
+                <Text>We are a dedicated team of professionals committed to excellence in every project we undertake. With years of experience and a passion for what we do, we strive to deliver top-notch services that meet our clients' unique needs and exceed their expectations. Our team's expertise, combined with our unwavering commitment to quality and customer satisfaction, sets us apart as a trusted name in our industry.</Text>
+                <Link to="/about">
+                  <CustomSolidButton buttonWidth="200px" buttonText="Find more about us" />
+                </Link>
+              </Stack>
+            </Flex>
+          </Box>
         </Stack>
       </Box>
 
-      <Box p={4} mt={5} background="#282C35" color="white" height={300} borderRadius="20px">
-      <Box>
-      <Center flexDirection={"column"} flexWrap={"wrap"}>
-        <Text textAlign={"center"}>What You Get</Text>
-        <Heading textAlign={"center"}>Our Services</Heading>
-        <HStack spacing={12} textAlign={"center"}>
-          <Box>
-          <Box display={"inline-flex"}> <FaLandmark color="#00840B" size={60}/></Box>
-          <Text> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet 
-          dolorum ipsum et beatae molestias dignissimos aliquid reprehenderit assumenda, quidem, nihil ducimus vitae.
-           Debitis maxime modi animi voluptatum autem corporis.
-         </Text>
-          </Box>
-          <Box>
-           <Box display={"inline-flex"}> <FaLandmark color="#00840B" size={60}/></Box>
-          <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet 
-          dolorum ipsum et beatae molestias dignissimos aliquid reprehenderit assumenda, quidem, nihil ducimus vitae.
-           Debitis maxime modi animi voluptatum autem corporis.
-          </Text>
-          </Box>
-          <Box>
-          <Box display={"inline-flex"} > <FaLandmark color="#00840B" size={60}/></Box>
-          <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet 
-          dolorum ipsum et beatae molestias dignissimos aliquid reprehenderit assumenda, quidem, nihil ducimus vitae.
-           Debitis maxime modi animi voluptatum autem corporis.
-          </Text>
-          </Box>
-        </HStack>
+      <Spacer height={50} />
+
+      <Box p={4} mt={5} background="#282C35" minH={"max-content"} borderRadius="20px">
+        <Center py={10} gap={3} flexDirection={"column"} flexWrap={"wrap"}>
+          <Text color="white" textAlign={"center"}>What You Get</Text>
+          <Heading color="white" textAlign={"center"}>Our Services</Heading>
+
+          <Flex gap={10}>
+            <ServicesCard serviceName={"Real estate management"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eius, soluta exercitationem ducimus inventore saepe nostrum placeat temporibus, dolorum maxime vero sapiente tempore rerum enim eum ab architecto, commodi distinctio."} cardIcon={BsHouse} />
+
+            <ServicesCard serviceName={"Real estate management"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eius, soluta exercitationem ducimus inventore saepe nostrum placeat temporibus, dolorum maxime vero sapiente tempore rerum enim eum ab architecto, commodi distinctio."} cardIcon={BsHouse} />
+
+            <ServicesCard serviceName={"Real estate management"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eius, soluta exercitationem ducimus inventore saepe nostrum placeat temporibus, dolorum maxime vero sapiente tempore rerum enim eum ab architecto, commodi distinctio."} cardIcon={BsHouse} />
+          </Flex>
         </Center>
       </Box>
-    </Box>
+
+
+      <Box mt={5} >
+        <Stack align="center" direction={{ base: 'column', md: 'row' }}>
+          <Box bgImage={Bg1} bgSize={"cover"} bgPosition={"center"} flex={1} height="500px" borderRadius="5px"></Box>
+          <Box p={4} flex={1} borderRadius="5px">
+            <Flex alignItems={"center"} flexDirection={"column"} textAlign={"start"}>
+              <Stack >
+                <Heading fontSize="20px">How it works</Heading>
+                <Accordion allowToggle>
+
+                  <Stack gap={2}>
+                    <AccordionItem>
+                      <h2>
+                        <CustomAccordionButton itemNumber={1} label={"Select a package"} />
+                      </h2>
+                      <AccordionPanel pb={4}>
+                        Select a package from our pricing table
+                      </AccordionPanel>
+                    </AccordionItem>
+
+                    <AccordionItem>
+                      <Heading >
+                        <CustomAccordionButton itemNumber={2} label="Complete payment form" />
+                      </Heading>
+                      <AccordionPanel pb={4}>
+                        Complete payment form and checkout
+                      </AccordionPanel>
+                    </AccordionItem>
+
+                    <AccordionItem width="402px">
+                      <h2>
+                        <CustomAccordionButton itemNumber={3} label={"Get your invoice"} />
+                      </h2>
+                      <AccordionPanel pb={4}>
+                        An invoice will be sent to you after proccessing your order which will contain your schedule information
+                      </AccordionPanel>
+                    </AccordionItem>
+                  </Stack>
+                </Accordion>
+              </Stack>
+            </Flex>
+          </Box>
+        </Stack >
+      </Box >
 
       <Box mt={8} textAlign="center" background="#282C35" color="#fff" borderRadius="10px" height={180}>
         <Center flexDirection="column" lineHeight={10}>
-        <Text>Get Started</Text>
-       <Heading >Ready to get started?</Heading> 
-        <Button mt={5}>
-          <a href="/Contact">Contact Us</a>
-        </Button>
+          <Text>Get Started</Text>
+          <Heading >Ready to get started?</Heading>
+          <Button mt={5}>
+            <a href="/Contact">Contact Us</a>
+          </Button>
         </Center>
       </Box>
-        </div>
-    )
+    </Box >
+  )
 }
