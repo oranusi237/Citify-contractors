84db7b01d450bd1f7a082ca3950d74b65839fe1a
oranusi237 <oranusisamuel98@gmail.com>
2024-04-21 07:23:10 -0700

    homepage

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Text, Stack, HStack, Center, Button, Flex, Icon, flexbox } from "@chakra-ui/react";
+import { Box, Heading, Text, Stack, HStack, Center, Button, Flex,  } from "@chakra-ui/react";
 import Bg from "../Assets/Images/pexel1.jpg"
 import Bg1 from "../Assets/Images/pexels-icsa-833425-1709003.jpg"
 import { FaLandmark } from "react-icons/fa6";
@@ -7,19 +7,19 @@ export default function Homepage() {
     return (
         <div>
          <Box p={4} bgImage={Bg} bgSize={"cover"} backgroundPosition={"center"} height="500px">
-         <Flex alignItems={"Center"} justifyContent={"Center"}>
-      <Stack spacing={8} color="#B80000">
+         <Flex  justifyContent={"Center"}>
+      <Stack spacing={8} color="#fff">
         <Center>
-          <Heading fontSize="25px"> Business</Heading>
+          <Heading> Business</Heading>
         </Center>
-        <Text fontSize="20px">We help you achieve your goals.</Text>
+        <Text fontSize="25px">We help you achieve your goals.</Text>
       </Stack>
       </Flex>
       </Box>
       <Box mt={5} >
         <Stack direction={{base: 'column', md: 'row'}}>
           <Box bgImage={Bg1} bgSize={"cover"} bgPosition={"center"} flex={1} height="300px" borderRadius="5px"></Box>
-          <Box bg="rgba(0, 0, 0, 0.5)" color="white" p={4} flex={1} borderRadius="5px">
+          <Box bg="#282C35" color="white" p={4} flex={1} borderRadius="5px">
             <Flex alignItems={"center"} flexDirection={"column"} textAlign={"center"}>
         <Heading as="h2">About Us</Heading>
         <Text fontSize="lg">
@@ -35,42 +35,48 @@ export default function Homepage() {
         </Stack>
       </Box>
 
-      <Box p={4} mt={5} background="rgba(0, 0, 0, 0.5)" color="white" height={300}>
+      <Box p={4} mt={5} background="#282C35" color="white" height={300} borderRadius="20px">
       <Box>
-      <Flex  justifyContent={"center"} flexDirection={"column"} flexWrap={"wrap"}>
-        <Heading variant="services" textAlign={"center"}>Our Services</Heading>
+      <Center flexDirection={"column"} flexWrap={"wrap"}>
+        <Text textAlign={"center"}>What You Get</Text>
+        <Heading textAlign={"center"}>Our Services</Heading>
         <HStack spacing={12} textAlign={"center"}>
           <Box>
-          <Box display={"inline-flex"}> <FaLandmark color="#B80000" size={60}/></Box>
+          <Box display={"inline-flex"}> <FaLandmark color="#00840B" size={60}/></Box>
           <Text> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet 
           dolorum ipsum et beatae molestias dignissimos aliquid reprehenderit assumenda, quidem, nihil ducimus vitae.
            Debitis maxime modi animi voluptatum autem corporis.
          </Text>
           </Box>
           <Box>
-           <Box display={"inline-flex"}> <FaLandmark color="#B80000" size={60}/></Box>
-          <Text variant="serviceDescription">
+           <Box display={"inline-flex"}> <FaLandmark color="#00840B" size={60}/></Box>
+          <Text>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet 
           dolorum ipsum et beatae molestias dignissimos aliquid reprehenderit assumenda, quidem, nihil ducimus vitae.
            Debitis maxime modi animi voluptatum autem corporis.
           </Text>
           </Box>
           <Box>
-          <Box display={"inline-flex"}> <FaLandmark color="#B80000" size={60}/></Box>
-          <Text variant="serviceDescription">
+          <Box display={"inline-flex"} > <FaLandmark color="#00840B" size={60}/></Box>
+          <Text>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet 
           dolorum ipsum et beatae molestias dignissimos aliquid reprehenderit assumenda, quidem, nihil ducimus vitae.
            Debitis maxime modi animi voluptatum autem corporis.
           </Text>
           </Box>
         </HStack>
-        </Flex>
+        </Center>
       </Box>
     </Box>
 
-      <Box mt={8} textAlign="center">
-        <Text>Ready to get started?</Text>
-        <Button>Contact Us</Button>
+      <Box mt={8} textAlign="center" background="#282C35" color="#fff" borderRadius="10px" height={180}>
+        <Center flexDirection="column" lineHeight={10}>
+        <Text>Get Started</Text>
+       <Heading >Ready to get started?</Heading> 
+        <Button mt={5}>
+          <a href="/Contact">Contact Us</a>
+        </Button>
+        </Center>
       </Box>
         </div>
     )
