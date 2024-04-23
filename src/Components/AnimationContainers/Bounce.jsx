236df85d656f4afdb37204a5f6fd37af7361e20a
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-23 12:13:14 +0100

    Initial commit

---
+++ src/Components/AnimationContainers/Bounce.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import { motion } from 'framer-motion'
+
+export default function BounceAnimationContainer(){
+    return(
+        <Box as={motion.div}>
+s
+        </Box>
+    )
+}
