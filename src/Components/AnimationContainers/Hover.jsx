0538591bb7e8641c9b1f1b52c9f8bd49b1f6367d
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-24 15:07:16 +0100

    Updating animation container

--- src/Components/AnimationContainers/Hover.jsx
+++ src/Components/AnimationContainers/Hover.jsx
@@ -4,8 +4,9 @@ import { motion } from "framer-motion"
 export default function HoverAnimationContainer({ children }) {
     return (
         <motion.div
-            whileHover={{ scale: 1.08 }}
-            whileTap={{ scale: 1 }}>
+            whileHover={{ scale: 1.1, opacity: 1.5 }}
+            initial={{ scale: 1, opacity: 1 }}
+            whileTap={{ scale: 1, opacity: 1 }}>
             {children}
         </motion.div>
     )
