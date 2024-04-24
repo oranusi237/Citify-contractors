0d9fb817051ff3c5e097f1bab6dbf8a3bbbb7d46
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-24 16:33:27 +0100

    updating animation styles

--- src/Components/AnimationContainers/Hover.jsx
+++ src/Components/AnimationContainers/Hover.jsx
@@ -5,8 +5,7 @@ export default function HoverAnimationContainer({ children }) {
     return (
         <motion.div
             whileHover={{ scale: 1.1, opacity: 1.5 }}
-            initial={{ scale: 1, opacity: 1 }}
-            whileTap={{ scale: 1, opacity: 1 }}>
+            initial={{ scale: 1, opacity: 1 }}>
             {children}
         </motion.div>
     )
