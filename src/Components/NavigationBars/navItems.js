06da261c83fcb32e43c25eea9074423661e783e4
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-25 20:04:35 +0100

    Updating ids for nav items

--- src/Components/NavigationBars/navItems.js
+++ src/Components/NavigationBars/navItems.js
@@ -1,7 +1,7 @@
 export const navItems = [
     { id: 1, name: 'About', path: "/about", Icon: '', },
-    { id: 1, name: 'Pricing', path: "/pricing", Icon: '', },
-    { id: 2, name: 'Contact', path: "/contact", Icon: '', },
-    { id: 3, name: 'Login', path: "/login", Icon: '', },
-    { id: 4, name: 'Sign up', path: "/sign-up", Icon: '', },
+    { id: 2, name: 'Pricing', path: "/pricing", Icon: '', },
+    { id: 3, name: 'Contact', path: "/contact", Icon: '', },
+    { id: 4, name: 'Login', path: "/login", Icon: '', },
+    { id: 5, name: 'Sign up', path: "/sign-up", Icon: '', },
 ]
