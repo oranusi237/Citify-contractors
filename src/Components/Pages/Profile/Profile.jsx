b5fcfc9433163503682e86cc8222989460a2051c
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-26 21:03:32 +0100

    Initial commit

---
+++ src/Components/Pages/Profile/Profile.jsx
@@ -0,0 +1,9 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export default function ProfilePage() {
+    return (
+        <Box>
+            <Heading>The profile page you know!!!</Heading>
+        </Box>
+    )
+}
