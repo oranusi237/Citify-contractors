0a0b52d04dc09f7175647a817256c91cbed4ea97
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-26 11:25:37 +0100

    Filtering login and sign up link for authenticated user

--- src/Components/NavigationBars/NavItemsRenderers.jsx
+++ src/Components/NavigationBars/NavItemsRenderers.jsx
@@ -2,11 +2,14 @@ import { Stack, Heading, } from "@chakra-ui/react"
 import { navItems } from "./navItems";
 import { NavLink } from "react-router-dom";
 import "./nav.css"
+import { useUserAuth } from "../Store/UserContext";
 
-export default function NavItemsRenderers({direction, color, onClick}) {
+export default function NavItemsRenderers({ direction, color, onClick }) {
+    const { isAuthenticated, user } = useUserAuth()
+    const filteredLink = navItems.filter((items) => items.name.toLowerCase() !== "login" && items.name.toLowerCase() !== "sign up")
     return (
         <Stack direction={direction} gap={5}>
-            {navItems.map((item) => (
+            {!user || user === null || !isAuthenticated ? navItems.map((item) => (
                 <NavLink onClick={onClick} className={({ isActive }) =>
                     isActive ? "active" : "in-active"
                 } key={item.id} to={item.path}>
@@ -14,7 +17,18 @@ export default function NavItemsRenderers({direction, color, onClick}) {
                         {item.name}
                     </Heading>
                 </NavLink>
-            ))}
+            ))
+                :
+                filteredLink.map((item) => (
+                    <NavLink onClick={onClick} className={({ isActive }) =>
+                        isActive ? "active" : "in-active"
+                    } key={item.id} to={item.path}>
+                        <Heading color={color} fontSize={20}>
+                            {item.name}
+                        </Heading>
+                    </NavLink>
+                ))}
+                {/* <Heading>{user!== null || user!== undefined ? user.email : ""}</Heading> */}
         </Stack>
     );
 }
