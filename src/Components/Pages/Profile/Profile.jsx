3d94f9bcbbd78901cf6f244cd788a4fbaccda336
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-27 23:54:07 +0100

    Populatin profile page with user information

--- src/Components/Pages/Profile/Profile.jsx
+++ src/Components/Pages/Profile/Profile.jsx
@@ -1,9 +1,53 @@
 import { Box, Heading } from "@chakra-ui/react";
+import { fdb, } from "../../Utils/Firebase/Firebase";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+
+
+
+// const App = () => {
+//   const [users, setUsers] = React.useState([]);
+
+//   React.useEffect(() => {
+//     usersRef.get().then((querySnapshot) => {
+//       const users = [];
+//       querySnapshot.forEach((doc) => {
+//         users.push(doc.data());
+//       });
+//       setUsers(users);
+//     });
+//   }, []);
+
+//   return <UserList users={users} />;
+// };
+
+
+
+
 
 export default function ProfilePage() {
+    const [userDetails, setUserDetails] = useState([])
+    useEffect(() => {
+        const querySnapshot = async () => {
+            const snapshot = await getDocs(collection(fdb, "Users"));
+            snapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.data());
+                setUserDetails(doc.data())
+                // console.log(userDetails.filter((item)=> item.firstName === "John"))
+
+            });
+        };
+
+        querySnapshot();
+    }, []);
+
     return (
+        // setTimeout(() => {
         <Box>
-            <Heading>The profile page you know!!!</Heading>
+            <Heading>{userDetails.firstName}</Heading>
+            <Heading>{userDetails.email}</Heading>
         </Box>
+        // }, 2000)
     )
 }
