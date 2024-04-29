8ff4229709917dd6faf0fb8f75076d05cfb0dfa0
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-04-29 19:32:22 +0100

    Adding logout button

--- src/Components/NavigationBars/NavItemsRenderers.jsx
+++ src/Components/NavigationBars/NavItemsRenderers.jsx
@@ -4,14 +4,21 @@ import { NavLink } from "react-router-dom";
 import "./nav.css"
 import { selectCurrentUser } from "../Store/user/userSelector";
 import { useSelector } from "react-redux";
+import { IoPower } from "react-icons/io5";
 
 export default function NavItemsRenderers({ direction, color, onClick }) {
+
+    const clearCache = () => {
+        // Clear browser cache by reloading the page
+        window.location.reload(true);
+      };
+
     const currentUser = useSelector(selectCurrentUser)
     console.log(currentUser)
     const filteredLink = navItems.filter((items) => items.name.toLowerCase() !== "login" && items.name.toLowerCase() !== "sign up")
     const signedOutUserLink = navItems.filter((item) => item.name.toLowerCase() !== "profile")
     return (
-        <Stack direction={direction} gap={5}>
+        <Stack justify="center" direction={direction} gap={5}>
             {currentUser === null || currentUser === undefined ? signedOutUserLink.map((item) => (
                 <NavLink onClick={onClick} className={({ isActive }) =>
                     isActive ? "active" : "in-active"
@@ -32,6 +39,8 @@ export default function NavItemsRenderers({ direction, color, onClick }) {
                     </NavLink>
                 ))}
             {/* <Heading>{user!== null || user!== undefined ? user.email : ""}</Heading> */}
-        </Stack>
+            {currentUser !== null && <IoPower fontSize={30} color="white" cursor="pointer" onClick={clearCache} />}
+
+        </Stack >
     );
 }
