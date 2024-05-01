23a820395e0ddb51caea2a6ec5b1919af4be298b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-05-01 11:17:22 +0100

    adding method to get current user

--- src/Components/Reuseables/Solidbutton.jsx
+++ src/Components/Reuseables/Solidbutton.jsx
@@ -1,10 +1,10 @@
 import { Box, Button } from "@chakra-ui/react";
 import { primaryColor } from "./colors";
 
-export default function CustomSolidButton({ otherProps, onClick, buttonWidth, hoverColor, type, buttonText, alignSelf, textColor, buttonColor }) {
+export default function CustomSolidButton({ loadingText, isLoading, onClick, buttonWidth, hoverColor, type, buttonText, alignSelf, textColor, buttonColor }) {
     return (
         <Box>
-            <Button onClick={onClick} _hover={{ background: hoverColor ? hoverColor : "#034C24" }} width={buttonWidth ? buttonWidth : "full"} type={type} fontWeight={900} color={textColor ? textColor : "white"} bg={buttonColor ? buttonColor : primaryColor} mt={5} alignSelf={alignSelf}>{buttonText}</Button>
+            <Button isLoading={isLoading} loadingText={loadingText} onClick={onClick} _hover={{ background: hoverColor ? hoverColor : "#034C24" }} width={buttonWidth ? buttonWidth : "full"} type={type} fontWeight={900} color={textColor ? textColor : "white"} bg={buttonColor ? buttonColor : primaryColor} mt={5} alignSelf={alignSelf}>{buttonText}</Button>
 
         </Box>
     )
