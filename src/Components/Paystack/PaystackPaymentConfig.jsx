b08da0a83225b01d2b9e6efae10a527d624b8ad4
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-05-03 20:51:27 +0100

    adding user metadata to payment object

--- src/Components/Paystack/PaystackPaymentConfig.jsx
+++ src/Components/Paystack/PaystackPaymentConfig.jsx
@@ -1,12 +1,44 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { PaystackButton } from "react-paystack"
-import { Box } from "@chakra-ui/react"
+import { Box, useToast } from "@chakra-ui/react"
+import "./styles.css"
+import { useSelector } from "react-redux"
+import { selectCurrentUser } from "../Store/user/userSelector"
+import { app, fdb } from "../Utils/Firebase/Firebase"
+import { getAuth } from "firebase/auth"
+import { collection, getDocs, query, where } from "firebase/firestore"
 
-export default function PaywithPaystack({email, name, phone}) {
+export default function PaywithPaystack({ amnt }) {
+
+    const [userDetails, setUserDetails] = useState([])
+    const currentUser = useSelector(selectCurrentUser)
+    const auth = getAuth(app)
+    const uid = currentUser.uid
+
+    const name = userDetails.firstName + userDetails.lastName
+    const email = userDetails.email
+    const phone = userDetails.phoneNumber
+
+    useEffect(() => {
+
+        const querySnapshot = async () => {
+            if (auth.currentUser !== null) {
+                const snapshot = await getDocs(query(collection(fdb, "Users"), where("userID", "==", uid)));
+                snapshot.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log(doc.data());
+                    setUserDetails(doc.data());
+                });
+            }
+        };
+
+        querySnapshot();
+    }, [auth.currentUser, uid]);
+
     const publicKey = process.env.REACT_APP_PAYSTACK_PUB_KEY
+    const toast = useToast()
+    const amount = amnt * 100
 
-    const amount = 1000000
- 
     const componentProps = {
         email,
         amount,
@@ -15,50 +47,16 @@ export default function PaywithPaystack({email, name, phone}) {
             phone,
         },
         publicKey,
-        text: "Pay Now",
+        text: "Confirm and Pay",
         onSuccess: () =>
-            alert("Thanks for doing business with us! Come back soon!!"),
+            toast({ title: "Payment Successful", description: "Your payment was successful, you will receive and email with confirmation of payment" }),
         onClose: () => alert("Are you sure you want to exit"),
     }
 
     return (
 
         <Box>
-            <PaystackButton text="Make payments now" {...componentProps} />
+            <PaystackButton className="payment-button" {...componentProps} />
         </Box>
-        // <div className="App">
-        //   <div className="container">
-        //     <div className="item">
-        //       <img />
-        //       <div className="item-details">
-        //         <p>Dancing Shoes</p>
-        //         <p>{amount}</p>
-        //       </div>
-        //     </div>
-        //     <div className="checkout-form">
-        //       <form>
-        //         <label>Name</label>
-        //         <input
-        //           type="text"
-        //           id="name"
-        //           onChange={(e) => setName(e.target.value)}
-        //         />
-        //         <label>Email</label>
-        //         <input
-        //           type="text"
-        //           id="email"
-        //           onChange={(e) => setEmail(e.target.value)}
-        //         />
-        //         <label>Phone</label>
-        //         <input
-        //           type="text"
-        //           id="phone"
-        //           onChange={(e) => setPhone(e.target.value)}
-        //         />
-        //       </form>
-        //       <PaystackButton {...componentProps} />
-        //     </div>
-        //   </div>
-        // </div>
     )
 }
