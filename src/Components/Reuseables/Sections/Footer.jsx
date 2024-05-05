10f1d4ddac0f28dd638102e70792ed89f0286d1f
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-05-05 20:55:29 +0100

    Adding path to footer links

--- src/Components/Reuseables/Sections/Footer.jsx
+++ src/Components/Reuseables/Sections/Footer.jsx
@@ -7,11 +7,11 @@ export default function Footer() {
         <footer>
             <Flex py="10px" bg="#F7F7F7" align="center" justify="space-around" direction="column" height={["200px", "200px", "150px"]} >
                 <Flex wrap="wrap" gap={2} justify="center" align="center">
-                    <Link >About</Link>
+                    <Link to={"/about"}>About</Link>
                     <Box borderRadius="50%" bg={primaryColor} height={1.5} width={1.5} display={["none", "none", "block"]}></Box>
-                    <Link >Contact</Link>
+                    <Link to={"/contact"} >Contact</Link>
                     <Box borderRadius="50%" bg={primaryColor} height={1.5} width={1.5} display={["none", "none", "block"]}></Box>
-                    <Link >Terms and conditions</Link>
+                    <Link to={"#"}>Terms and conditions</Link>
                 </Flex>
                 <Divider />
                 <Flex justify="center" align="center">
