ae693ad7ec5e6ce616943c5e27ed235111111768
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-05-07 22:43:35 +0100

    Updating user auth dependencies

--- src/Components/Utils/NoAuth.jsx
+++ src/Components/Utils/NoAuth.jsx
@@ -1,19 +1,18 @@
 import { Box } from "@chakra-ui/react";
-import { useSelector } from "react-redux";
-import { selectCurrentUser } from "../Store/user/userSelector";
-import { selectAuth } from "../Store/user/authStatusSelector";
 import { Navigate, useLocation } from "react-router-dom";
 
 export default function HideOnAuth({ childComponent }) {
-    const currentUser = useSelector(selectCurrentUser)
     const location = useLocation()
-    const isAuthenticated = useSelector(selectAuth)
+    const token = localStorage.getItem("auth-token")
     return (
-        isAuthenticated || currentUser ?
-            <Navigate to="/profile" state={{ from: location }} replace />
-            :
-            <Box>
-                {childComponent}
-            </Box>
+        <Box>
+            {token ?
+                <Navigate to="/profile" state={{ from: location }} replace />
+                :
+                <Box>
+                    {childComponent}
+                </Box>
+            }
+        </Box>
     )
 }
--- src/Components/Utils/RequireAuth.jsx
+++ src/Components/Utils/RequireAuth.jsx
@@ -1,14 +1,10 @@
 import { Box } from "@chakra-ui/react";
 import { Navigate, useLocation } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectCurrentUser } from "../Store/user/userSelector";
-import { selectAuth } from "../Store/user/authStatusSelector";
 
 
 export default function RequireAuth({ childComponent }) {
     const location = useLocation()
-    const currentUser = useSelector(selectCurrentUser)
-    const isAuthenticated = useSelector(selectAuth)
+    const token = localStorage.getItem("auth-token")
 
-    return isAuthenticated || currentUser ? <Box>{childComponent}</Box> : <Navigate to="/login" state={{ from: location }} replace />
+    return token ? <Box>{childComponent}</Box> : <Navigate to="/login" state={{ from: location }} replace />
 }
